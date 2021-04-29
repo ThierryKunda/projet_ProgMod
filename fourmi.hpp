9c958b36be43d4af19d69919e00399bca7284193
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2021-04-29 15:07:11 +0200

    Classe Fourmi

--- fourmi.hpp
+++ fourmi.hpp
@@ -5,5 +5,23 @@
 #include <vector>
 #include "coord.hpp"
 
+class Fourmi {
+    // Attributs
+    Coord coords;
+    int num;
+
+    public:
+
+    // Constructeur
+    Fourmi(Coord crds, int indice);
+
+    // Méthodes
+    Coord get_coords() const {return coords;}
+    void set_coords(Coord crds) {coords = crds;}
+    void set_coords(int lig, int col) {coords = {lig, col};}
+    bool porteSucre() const;
+    void prendSucre();
+    void deplace();
+};
 
 #endif // FOURMI_HPP
