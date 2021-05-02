e8d2ed32a9b58d56c979f6f67485132ee2855418
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2021-05-02 15:36:05 +0200

    Déf. de chargePlace()

--- grille.cpp
+++ grille.cpp
@@ -6,3 +6,12 @@ using namespace std;
 
 /* Constructeur vide de Grille */
 Grille::Grille() : places{{}} {}
+
+/* Getter (Place)
+@return Place */
+Place Grille::chargePlace(Coord crds) const {
+	for (Place p: places) {
+		if (crds == p.get_coord())
+			return p;
+	}
+}
