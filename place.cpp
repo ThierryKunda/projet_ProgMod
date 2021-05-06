58370bbd3218ad89857e7167fc45037cdaaa9dab
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2021-05-06 14:42:41 +0200

    Début tests

--- place.cpp
+++ place.cpp
@@ -23,3 +23,34 @@ bool estPlusProcheNid(const Place p1, const Place p2) {
     return (p1.get_pheroNid() >= p2.get_pheroNid());
 }
 
+// TESTS
+
+TEST_CASE("Getter des coordonnées") {
+	Place p = {Coord{2,3}};
+	CHECK(p.get_coord() == Coord{2,3});
+}
+
+TEST_CASE("Méthode contientSucre") {
+	Place p = {Coord{2,3}};
+	CHECK(!(p.contientSucre()));
+	p.poseSucre();
+	CHECK(p.contientSucre());
+}
+
+TEST_CASE("Méthode contientFourmi (optionnel)") {
+	Place p = {Coord{2,3}};
+	CHECK(!(p.contientFourmi()));
+	Fourmi fourmi = {Coord{1,1}, 3};
+	p.poseFourmi(fourmi);
+	CHECK(p.contientFourmi());
+	p.enleveFourmi();
+	CHECK(!(p.contientFourmi()));
+}
+
+TEST_CASE("Méthode posePheroNid/Sucre") {
+	Place p = {Coord{2,3}};
+	p.posePheroNid(0.2);
+	p.posePheroSucre();
+	CHECK(p.get_pheroNid() == 0.2);
+	CHECK(p.estSurUnePiste());
+}
