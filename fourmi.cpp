f51576ef93432b44dcf9a0a2b6a6bd549d3e9afc
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2021-05-18 17:16:57 +0200

    déf. constructs Colonie

--- fourmi.cpp
+++ fourmi.cpp
@@ -8,3 +8,10 @@ using namespace std;
 Fourmi::Fourmi(Coord crds, int indice) : coords{crds}, num{indice}, sucre{false}{
     if (num < 0) {throw("Indice négatif : non valide.");}
 }
+
+// Constructeurs
+Colonie::Colonie() : fourmis{{}}, indices{{}} {}
+
+Colonie::Colonie(vector<Fourmi> col) : fourmis{col}, indices{{}} {
+    for (Fourmi f: col) {indices.push_back(f.get_num());}
+}
