9f9b7f15f69a95f832bb451e11e3e3cbf7677e02
Thierry Kunda <thierry.kunda@universite-paris-saclay.fr>
2021-05-20 11:55:32 +0200

    Correctifs

--- place.cpp
+++ place.cpp
@@ -15,7 +15,7 @@ using namespace std;
  * @param Place d'arrivée
  */
 void deplaceFourmi(Fourmi &f, Place &p1, Place &p2) {
-    p1.enleveFourmi(f);
+    p1.enleveFourmi();
     f.deplace(p2.get_coord());
     p2.poseFourmi(f);
 }
